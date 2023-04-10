10e355b6cb164b3ae0ff04a02cfd6bfc6afe0f82
sulli <sullicla@uw.edu>
2023-04-10 16:21:59 -0700

    TCP Socket Client Update 1

---
+++ TCPClient.java
@@ -0,0 +1,23 @@
+import java.net.*;
+import java.io.*;
+
+public class TCPClient {
+    public static void main(String... args) {
+        String host = args[0];
+        int port = Integer.valueOf(args[1]);
+
+        try {
+
+            Socket sock = new Socket(host, port);
+
+            InputStream in = sock.getInputStream();
+            int readChar = 0;
+            while ((readChar = in.read()) != -1) {
+                System.out.write(readChar);
+            }
+            sock.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
